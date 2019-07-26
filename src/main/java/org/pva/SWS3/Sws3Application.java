eaec937bd4b73a7ed22c5b559c23690014dba6f3
ViktorVx <victorptrv@yandex.ru>
2019-07-26 11:00:03 +0300

    First commit

---
+++ src/main/java/org/pva/SWS3/Sws3Application.java
@@ -0,0 +1,14 @@
+package org.pva.SWS3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sws3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sws3Application.class, args);
+	}
+
+}
---
+++ src/main/java/org/pva/SWS3/controller/RestController.java
@@ -0,0 +1,7 @@
+package org.pva.SWS3.controller;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+
+}
